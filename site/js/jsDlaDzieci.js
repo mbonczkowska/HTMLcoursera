d89de1bab05cf80b259341698f7abb21c45051c3
Magda Bonczkowska <magda1231@gmail.com>
2017-04-27 20:26:57 +0200

    obiekty

--- site/js/jsDlaDzieci.js
+++ site/js/jsDlaDzieci.js
@@ -28,4 +28,30 @@ $(document).ready(function(){
 	let numbers = [3,"jest większe niż",2,"jest większe niż",1];
 	$(".greaterThen").html(numbers.join(" "));
 
+
+	let scores = {};
+
+	scores.magda = 0;
+	scores.kasia = 0;
+	scores.ania = 0;
+	scores.pawel = 0;
+	scores.wojtek = 0;
+
+	// round 1
+	scores.magda = 1;
+
+	// add point to every gamer
+	for(let i = 0; i < Object.keys(scores).length; i++){
+		scores[Object.keys(scores)[i]] += 1;
+		$(".score").append(Object.keys(scores)[i] + ": " + scores[Object.keys(scores)[i]] + " ");
+	}
+
+	var moj = {
+		"nazwa":"moja",
+ 		"tab": [ 7, 9, {cel: "zamieszkanie", liczba: 123}, 3.3]
+ 	};	
+
+ 	$(".getNumber").html(moj["tab"][2]["liczba"]);
+
+
 });
